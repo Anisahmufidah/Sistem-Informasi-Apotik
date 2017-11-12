9371b78e913e40482db95ea525bc9063e0cb29e9
akmalfdy <akml.fuady@gmail.com>
2017-11-12 08:52:01 +0700

    add class transaksi detail

--- src/apotek/Transaksi.java
+++ src/apotek/Transaksi.java
@@ -1,19 +1,28 @@
 package apotek;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Transaksi {
     private String id;
     private Pembeli pembeli;
     private Kasir kasir;
     private ArrayList<Obat> obat;
+    private ArrayList<TransaksiDetail> trxDetail = new ArrayList();
+    private Date tglTrx;
     private int jumObat;
-    private double totalharga;
+    private double totalharga = 0;
     
     public Transaksi(String id, Pembeli pembeli, Kasir kasir) {
         this.id = id;
         this.pembeli = pembeli;
         this.kasir = kasir;
+    }
+    
+    public void beli(Obat obat){
+        this.trxDetail.add(new TransaksiDetail(this, obat, 1, obat.getHarga()));
     }
 
     public void setId(String id) {
@@ -61,7 +70,31 @@ public class Transaksi {
     }
 
     public double getTotalharga() {
-        return totalharga;
+        for(TransaksiDetail detail: trxDetail){
+            this.totalharga = this.totalharga + detail.getHarga();
+        }
+        
+        return this.totalharga;
+    }
+    
+    public void buktiPembayaran(){
+        System.out.println("Bukti Pembayaran\n\n");
+        System.out.println("Tanggal : " + this.getCurrentDate() + "\n\n");
+        System.out.println("-------------------------------------------");
+        System.out.println("Nama Obat \tQty \t\tHarga");
+        System.out.println("-------------------------------------------");
+        for(TransaksiDetail detail: trxDetail){
+            System.out.println(detail.getObat().getNama() + "\t" + detail.getQty() + "\t\t" + detail.getHarga());
+        }
+        System.out.println("-------------------------------------------");
+        System.out.println("\t\tTotal Harga : \t" + this.getTotalharga());
+    }
+    
+    public String getCurrentDate(){
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        
+        return dateFormat.format(date);
     }
     
     @Override
